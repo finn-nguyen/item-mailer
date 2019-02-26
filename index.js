5c1df2635e58fffb6d663905d836b0768f73db6b
nvtuongcs <nvtuongcs@gmail.com>
2019-02-26 16:55:30 +0700

    Using plain login

--- index.js
+++ index.js
@@ -1,31 +1,24 @@
 const nodemailer = require('nodemailer');
 const schedule = require('node-schedule');
 const readLastLines = require('read-last-lines');
-require('xoauth2');
 require('dotenv').config();
 
 var transporter = nodemailer.createTransport({
-  host: 'smtp.gmail.com',
-    port: 465,
-    secure: true,
+  service: "Gmail",
     auth: {
-        type: 'OAuth2',
-        user: process.env.USER_EMAIL,
-        clientId: process.env.CLIENT_ID,
-        clientSecret: process.env.CLIENT_SECRECT,
-        refreshToken: process.env.REFRESH_TOKEN,
-        accessToken: process.env.ACCESS_TOKEN
+      user: process.env.USER_EMAIL,
+      pass: process.env.PASS
     }
 });
 
 var mailOptions = {
-  from: 'Tuong Nguyen',
+  from: `Tuong Nguyen <${process.env.USER_EMAIL}>`,
   to: process.env.RECEIVER,
   subject: 'Sending Email using Node.js',
   text: 'Hello WOrld'
 };
 
-const job = schedule.scheduleJob('53 * * * *', function() {
+const job = schedule.scheduleJob('40 * * * *', function() {
   const lastLine = readLastLines.read('test.txt', 1)
     .then((line) => {
       const content = {...mailOptions, text: line };
